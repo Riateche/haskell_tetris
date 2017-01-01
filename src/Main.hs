ca92b1c64541546b39aadac82b6ec64cbea9f5e4
Ri <ri@idzaaus.org>
2017-01-01 19:46:53 +0300

    more if

--- src/Main.hs
+++ src/Main.hs
@@ -8,12 +8,13 @@ put_field field = do
   putStrLn (show_field field)
   hFlush stdout
 
-start_new_figure field = check_new_figure (add_figure field (generate_figure "test"))
-
-check_new_figure field | has_collision field = do
-  put_field field
-  putStrLn "Game Why No Space Over"
-check_new_figure field = query_command field
+start_new_figure field = do
+ let field2 = add_figure field (generate_figure "test")
+ if has_collision field2
+   then do
+     put_field field2
+     putStrLn "Game Why No Space Over"
+   else query_command field2
 
 query_command field = do
   put_field field
@@ -22,15 +23,14 @@ query_command field = do
   line <- getLine
   let new_field = apply_command (field) line
   put_field new_field
-  check_fall new_field
+  if can_fall new_field
+    then query_command (step_fall new_field)
+    else do
+      let f = complete_fall new_field
+      put_field f
+      start_new_figure f
 
-check_fall field | can_fall field = query_command (step_fall field)
-check_fall field = do
-  let f = complete_fall field
-  put_field f
-  start_new_figure f
 
 
 main :: IO ()
-main = do
-  start_new_figure empty_field
+main = start_new_figure empty_field
